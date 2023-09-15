a233da7813130b6a3f85edd99f9d105c4bcc0c89
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2023-09-15 14:50:16 -0300

    feat: drive upload

---
+++ src/utils/driveUpload.ts
@@ -0,0 +1,54 @@
+import fs from "node:fs";
+import { google } from "googleapis";
+import path from "node:path";
+
+export async function driveUpload(name: string) {
+  try {
+    const privateKey = process.env.PRIVATE_KEY!.replace(/\n/g, "\\n");
+
+    console.log(privateKey);
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        type: process.env.TYPE!,
+        private_key: process.env.PRIVATE_KEY!.replace(/\n/g, "\n"),
+        universe_domain: process.env.UNIVERSE_DOMAIN,
+      },
+      projectId: process.env.PROJECT_ID,
+      scopes: ["https://www.googleapis.com/auth/drive"],
+    });
+
+    // const auth = new google.auth.JWT(
+    //   process.env.CLIENT_EMAIL,
+    //   undefined,
+    //   process.env.PRIVATE_KEY!.replace(/\n/g, "\n"),
+    //   "https://www.googleapis.com/auth/drive"
+    // );
+    // console.log("⚡", auth);
+    // const g = await auth.authorize();
+    // console.log("asdasda", g);
+    const driveService = google.drive({
+      version: "v3",
+      auth,
+    });
+    const fileMetaData = {
+      name: name,
+      parents: [process.env.GOOGLE_API_FOLDER_ID!],
+    };
+    const readDestination = path.resolve(__dirname, "../../tmp", name);
+    console.log(readDestination);
+    const media = {
+      mimeType: "audio/mpeg",
+      body: fs.createReadStream(readDestination),
+    };
+    const response = await driveService.files.create({
+      requestBody: fileMetaData,
+      media: media,
+      fields: "id",
+    });
+    return response.data.id;
+  } catch (error) {
+    console.log(error);
+  }
+}
