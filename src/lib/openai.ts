d8c5ff5def8ea2fb5ad6c0b1cc885da36445bada
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2023-12-05 20:15:26 -0300

    chore: add runtime

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,6 +1,8 @@
 import { OpenAI } from "openai";
 import { Configuration, OpenAIApi } from "openai-edge";
 
+export const runtime = "edge";
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_KEY,
 });
